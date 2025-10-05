9037819ef5f94afa9fcbaa39d2f6b330f70dbc0f
NadimJebali <nadimjebali.0@gmail.com>
2025-10-05 18:00:17 +0100

    seeting up few components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'splash',
+    component: SplashScreenComponent
+  },
+  {
+    path: 'userDetails',
+    component: UserDetailsComponent
+  },
+  {
+    path: 'Profile',
+    component: UserProfileComponent
   },
   {
     path: '',
